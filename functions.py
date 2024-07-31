770aa5d4d96d872239eb78f2064c16cd6bdadbb8
rodnilo <nilo.rodrigo01@gmail.com>
2024-07-31 16:27:24 -0300

    xRevert "creating repo"
    This reverts commit 36c49e312c9fa538d0a2855bfd6c36227ab73a46.

--- functions.py
+++ functions.py
@@ -65,12 +65,10 @@ def inactivate(email, token):
     response_patch = requests.patch(endpoint_inactivate, headers=headers, json=body)
     return response_patch.json()
 
-
 def register(name, email, token):
     body = {
         "name": name,
         "email": email,
-        "password": "123456",
         "sendConfirmationEmail": "true"
     }
 
@@ -89,6 +87,71 @@ def register(name, email, token):
     else:
         return response_json
 
+def find_id_by_title(contents, target_titles):
+    """
+    função que tem o objetivo de buscar os títulos dos conteúdos selecionados no typeform para retornar o id (int)
+    dos cursos na curseduca
+
+    :param contents: recebe a lista de conteúdo resultante da função get_contents
+    :param target_titles: recebe do zapier a lista de conteúdos selecionados do typeform tratados, em formato de lista de string
+    :return: retorna lista de ids
+    """
+    contents_comma = []
+    for string in target_titles:
+        nova_string = string.replace("/", ",")
+        contents_comma.append(nova_string)
+    target_titles = contents_comma
+    id_map = {item['title']: item['id'] for item in contents}
+    return [id_map.get(title, None) for title in target_titles]
+
+def find_uuid_by_title(contents, target_titles):
+    """
+    função que tem o objetivo de buscar os títulos dos conteúdos selecionados no typeform para retornar o uuid (string)
+    dos cursos na curseduca
+
+    :param contents: recebe a lista de conteúdo resultante da função get_contents
+    :param target_titles: recebe do zapier a lista de conteúdos selecionados do typeform tratados, em formato de lista de string
+    :return: retorna lista de uuids
+    """
+    contents_comma = []
+    for string in target_titles:
+        nova_string = string.replace("/", ",")
+        contents_comma.append(nova_string)
+    target_titles = contents_comma
+    id_map = {item['title']: item['uuid'] for item in contents}
+    return [id_map.get(title, None) for title in target_titles]
+
+def get_contents(token):
+    """
+    tem o objetivo de retornar a lista de dicionários dos conteúdos, contendo titulo, id, uuid, slug entre outros dados
+    para ser consumido pelas funções de busca de títulos
+    :param token: token de acesso retornado pela função auth
+    :return: retorna lista de dicionários para serem consumidos pelas funções de busca de títulos
+    """
+
+    body = {
+        "username": username,
+        "password": password,
+    }
+
+    headers = {
+        "api_key": api_key,
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    # recebe resposta
+    response_get = requests.get(
+        endpoint_contents,
+        headers=headers,
+        json=body
+    )
+
+    # torna json legivel
+    data = response_get.json()
+    data = data['data']
+    return data
+
 def enroll_group(token, id, group_id):
     """
     Função que tem objetivo atribuir o usuário criado pela função register_with_contents à respectiva turma da empresa.
@@ -101,7 +164,6 @@ def enroll_group(token, id, group_id):
     body = {
         "member": {"id": id},
         "group": {"id": group_id},
-        "customExpirationDate": "XXX"
     }
 
     headers = {
@@ -136,3 +198,167 @@ def enroll_tenant(token, id, tenant_uuid):
     response_post = requests.post(endpoint_tenants, headers=headers, json=body)
 
     return response_post
+
+def identify(email):
+    """
+    Função que tem como objetivo identificar quem é o usuário que será utilizado nas ações de atribuir novos conteúdos
+    a um colaborador existente e de remoção de colaborador
+    :param email: email a ser identificado
+    :return: id do usuário
+    """
+    endpoint_busca = endpoint_identify + f"by?email={email}"
+
+    response_get = requests.get(endpoint_busca, headers=headers)
+    response_json = response_get.json()
+    member_id = response_json['id']
+    if type(member_id) is int:
+        return member_id
+    else:
+        return response_json
+
+def enroll(member_id, conteudos_selecionados, conteudos, token):
+    """
+    Função com o objetivo de matricular um usuário existente em um ou mais conteúdos, passando o member_id e o(s) content_id(s)
+    :param member_id: id do usuário que foi buscado na função identify
+    :param conteudos_selecionados: lista de conteúdos selecionados no typeform
+    :param conteudos: conteudos resultantes da função get_contents para serem buscados
+    :param token: token resultado da função auth
+    :return: body da chamada
+    """
+
+    content_ids = find_id_by_title(conteudos, conteudos_selecionados)
+    headers = {
+        "api_key": api_key,
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    enrollments = []
+    counter = 0
+    for i in content_ids:
+        body = {
+            "member": {
+                "id": member_id
+            },
+            "contentId": i
+        }
+
+        response_post = requests.post(endpoint_enrollments, headers=headers, json=body)
+
+        response_json = response_post.json()
+        if type(response_json) is int:
+            enrollments.append(response_json)
+        else:
+            pass
+        counter += 1
+    if len(enrollments) == counter:
+        return enrollments
+    else:
+        return response_json
+
+def revoke(enrollments, token):
+    """
+    Função que revoga o acesso dos cursos que devem ser bloqueados ao usuário.
+    :param enrollments: lista de ids dos cursos que devem ser bloqueados
+    :param token: token resultado da função auth
+    :return: lista de matrículas revogadas
+    """
+
+    enrollments.pop(0)
+    print(enrollments)
+    for i in enrollments:
+        endpoint = f"https://clas.curseduca.pro/enrollments/{i}"
+        headers = {
+            "api_key": api_key,
+            "Authorization": f"Bearer {token}",
+            "accept": "application/json"
+        }
+        response = requests.delete(endpoint, headers=headers)
+    return enrollments
+
+def get_enrollments(token):
+    """
+    Função que retorna lista de dicionários com dados de matrícula
+    :param token: token de acesso
+    :return: lista de enrollments
+    
+    """    
+
+    headers = {
+        "api_key": api_key,
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    body = {
+        "username": username,
+        "password": password,
+    }
+
+    enrollments = requests.get(
+        endpoint_enrollments_report, 
+        headers=headers,
+        json=body
+    )
+
+    data = enrollments.json()
+    data = data['data']
+    return data
+
+def identify_users_enrollments(enrollments, member_id, content_ids):
+    """
+    Função com o objetivo de identificar as matrículas dos colaboradores com base no input de enrollments, member_id e content_ids.
+    :param member_id: id do usuário que foi buscado na função identify
+    :param content_ids: lista de conteúdos selecionados no typeform
+    :param enrollments: resultado da chamada get_enrollments que retorna todas as matrículas
+    :return: lista de enrollments associada ao membro e aos conteúdos
+    
+    """    
+    id_map = {enrollment['content']['id']: enrollment['id'] for enrollment in enrollments if enrollment['member']['id'] == member_id}
+    print(id_map)
+    enrollments_raw = [id_map.get(content_id) for content_id in content_ids]
+    enrollments = [x for x in enrollments_raw if x is not None]
+    return enrollments
+
+def restore_enrollments(enrollments, token):
+    """
+    Função com o objetivo de liberar conteúdos das trilhas cadastradas. Objetivamente reativa uma matrícula revogada.
+    :param member_id: id do usuário que foi buscado na função identify
+    :param content_ids: lista de conteúdos selecionados no typeform
+    :param token: token resultado da função auth
+    :return: body da chamada
+    """
+
+    headers = {
+        "api_key": api_key,
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    body = {
+        "username": username,
+        "password": password,
+    }
+
+    for i in enrollments:
+        endpoint = endpoint_restore_enrollments + f"/{i}/restore"
+        response = requests.patch(endpoint, headers=headers, json=body)
+        return enrollments
+
+def revoke_all(enrollments, token):
+    """
+    Função que revoga o acesso dos cursos que devem ser bloqueados ao usuário.
+    :param enrollments: lista de ids dos cursos que devem ser bloqueados
+    :param token: token resultado da função auth
+    :return: lista de matrículas revogadas
+    """
+
+    for i in enrollments:
+        endpoint = f"https://clas.curseduca.pro/enrollments/{i}"
+        headers = {
+            "api_key": api_key,
+            "Authorization": f"Bearer {token}",
+            "accept": "application/json"
+        }
+        response = requests.delete(endpoint, headers=headers)
+    return enrollments
--- script.py
+++ script.py
@@ -16,30 +16,134 @@ endpoint_restore_enrollments = os.getenv("endpoint_restore_enrollments")
 url = os.getenv("url")
 
 from functions import auth
+from functions import find_id_by_title
+from functions import enroll
 from functions import enroll_tenant
 from functions import enroll_group
+from functions import identify
+from functions import get_contents
+from functions import revoke
 from functions import register
+from functions import get_enrollments
+from functions import identify_users_enrollments
+from functions import restore_enrollments
+from functions import inactivate
+from functions import revoke_all
 
-name = input_data['name']
-email = input_data['email']
-tenant_uuid = input_data['tenant_uuid']
-group_id = input_data['group_id']
+if input_data['action'] == 'update':
+    try:
+        print(f"Operação a ser realizada: {input_data['action']} \n 0/4 etapas concluídas")
+        token = auth()
+        print(f"Autenticação realizada. \n 1/4 etapas")
+        email = input_data['email_update']
+        print(f"E-mail a ser atualizado: {email}")
+        conteudos_selecionados = input_data['contents_update'] #.split(",") # esta e a próxima linha precisam ser descomentadas para funcionar corretamente no Zapier
+        # conteudos_selecionados = [i.strip() for i in conteudos_selecionados]
+        print(f"Conteúdos selecionados para serem matriculados: {conteudos_selecionados} \n 2/4 etapas")
+        conteudos = get_contents(token)
+        member_id = identify(email)
+        print(f"ID do membro {email}: {member_id}. \n 3/4 etapas")
+        status_enroll = enroll(member_id, conteudos=conteudos, conteudos_selecionados=conteudos_selecionados, token=token)
+        print(f"Matrículas realizadas: {status_enroll}. \n 4/4 etapas.")
+    except KeyError as e:
+        print(e)
+
+elif input_data['action'] == 'delete':
+    try:
+        print(f"Operação a ser realizada: {input_data['action']}. \n 0/2 etapas concluídas")
+        token = auth()
+        print(f"Autenticação realizada. \n 1/2 etapas")
+        email = input_data['email_delete']
+        print(f"Email a ser desativado: {email}")
+        response_inactivate = inactivate(email, token)
+        print(f"Resposta ação {input_data['action']}: {response_inactivate}. \n 2/2 etapas")
+
+    except KeyError as e:
+        print(e)
+
+elif input_data['action'] == 'create_track_new_user':
+    try:
+        print(f"Operação a ser realizada: {input_data['action']}. \n 0/5 etapas concluídas")
+        token = auth()
+        print(f"Autencicação realizada. \n 1/5 etapas")
+        name = input_data['name_create_track']
+        email = input_data['email_create_track_new']
+        group_id = input_data['group_id']
+        tenant_uuid = input_data['tenant_uuid']
+        conteudos_selecionados = input_data['contents_create_track_new']#.split(",") # esta e a próxima linha precisam ser descomentadas para funcionar corretamente no Zapier
+        # conteudos_selecionados = [i.strip() for i in conteudos_selecionados]
+        print(f"Nome a ser criado: {name} \n E-mail a ser criado: {email} \n Conteúdos da trilha: {conteudos_selecionados}")
+        conteudos = get_contents(token)
+        member_id = register(name=name, email=email, token=token)
+        print(f"Usuário gerado: {member_id} \n 1/X etapas concluídas")
+        response_enroll_group = enroll_group(id=member_id, group_id=group_id, token=token)
+        print(f"Usuário {member_id} inserido na turma {group_id}. \n Resposta da solicitação: {response_enroll_group} \n 2/5 etapas concluídas")
+        response_enroll_tenant = enroll_tenant(id=member_id, tenant_uuid=tenant_uuid, token=token)
+        print(f"Usuário {member_id} inserido no tenant {tenant_uuid} \n Resposta da solicitação: {response_enroll_tenant} \n 3/5 etapas concluídas")
+        enrolled_contents = enroll(member_id=member_id, conteudos_selecionados=conteudos_selecionados, conteudos=conteudos, token=token)
+        print(f"{len(enrolled_contents)} matrículas realizadas. Os ids são: {enrolled_contents} \n 4/5 etapas concluídas")
+        revoked_contents = revoke(enrolled_contents, token=token)
+        conteudos_selecionados.pop(0)
+        print(f"Os conteúdos: \n {conteudos_selecionados} \n foram bloqueados para o aluno {name} identificador {member_id}. \n 5/5 etapas concluídas. \n Operação realizada com sucesso.")
+
+    except KeyError as e:
+        print(e)
+
+elif input_data['action'] == 'create_track_existing_user':
+    try:
+        print(f"Operação a ser realizada {input_data['action']}. \n 0/4 etapas concluídas.")
+        token = auth()
+        email = input_data['email_create_track_existing']
+        conteudos_selecionados = input_data['contents_create_track_existing']#.split(",") # esta e a próxima linha precisam ser descomentadas para funcionar corretamente no Zapier
+        # conteudos_selecionados = [i.strip() for i in conteudos_selecionados]
+        print(f"Conteúdos selecionados: \n {conteudos_selecionados} \n 1/4 etapas concluídas")
+        conteudos = get_contents(token)
+        member_id = identify(email=email)
+        print(f"Email existente: {email}. Identificador: {member_id} \n 2/4 etapas concluídas")
+        enrolled_contents = enroll(member_id=member_id, conteudos=conteudos, conteudos_selecionados=conteudos_selecionados, token=token)
+        print(f"{len(enrolled_contents)} matrículas realizadas. Os ids são: {enrolled_contents} \n 3/4 etapas concluídas")
+        revoked_contents = revoke(enrolled_contents, token=token)
+        conteudos_selecionados.pop(0)
+        print(f"Os conteúdos: \n {conteudos_selecionados} \n foram bloqueados para o aluno {email} identificador {member_id}. \n Etapa 4/4 realizada")
+    except KeyError as e:
+        print(e)
+
+elif input_data['action'] == 'release_track':
+    try:
+        print(f"Operação a ser realizada {input_data['action']}. \n 0/3 etapas concluídas.")
+        token = auth()
+        email = input_data['email_release_track']
+        conteudos_selecionados = input_data['contents_release']#.split(",") # esta e a próxima linha precisam ser descomentadas para funcionar corretamente no Zapier
+        # conteudos_selecionados = [i.strip() for i in conteudos_selecionados]
+        conteudos = get_contents(token)
+        member_id = identify(email=email)
+        print(f"Email existente: {email}. Identificador: {member_id}. \n 1/3 etapas concluídas")
+        enrollments = get_enrollments(token)
+        content_ids = find_id_by_title(conteudos, conteudos_selecionados)
+        print(content_ids)
+        member_enrollments = identify_users_enrollments(enrollments, member_id=member_id, content_ids=content_ids)
+        print(f"{len(member_enrollments)} identificado para liberação, identificador {member_enrollments}. \n 2/3 etapas concluídas")
+        restored_enrollments = restore_enrollments(member_enrollments, token=token)
+        print(f"{len(restored_enrollments)} identificado para liberação, identificador {member_enrollments}. \n 3/3 etapas concluídas")
+    except KeyError as e:
+        print(e)
 
 
-token = auth()
-try:
-    member_id = register(name=name, email=email, token=token)
-    print(f"Membro cadastrado: \n Nome: {name} \n E-mail: {email} \n Identificador: {member_id} \n Etapa 1/3 realizada")
-except TypeError or KeyError as e:
-    print("Erro ao cadastrar o membro. Erro: {e}")
-
-try:
-    enroll_tenant(id=member_id, token=token, tenant_uuid=tenant_uuid)
-    print(f"Tenant {tenant_uuid} cadastrado para o membro {member_id} \n Etapa 2/3 realizada")
-except TypeError or KeyError as e:
-    print(f"Erro ao cadastrar tenant {tenant_uuid} para o membro {member_id}. Erro: {e}")
-try:
-    enroll_group(id=member_id, token=token, group_id=group_id)
-    print(f"Grupo {group_id} cadastrado para o membro {member_id} \n Etapa 3/3 realizada")
-except TypeError or KeyError as e:
-    print(f"Erro ao cadastrar grupo {group_id} para o membro {member_id}. Erro: {e}")
+elif input_data['action'] == 'revoke_all':
+    try:
+        print(f"Operação a ser realizada {input_data['action']}. \n 0/3 etapas concluídas.")
+        token = auth()
+        email = input_data['email_revoke_all']
+        conteudos_selecionados = input_data['contents_to_revoke']#.split(",") # esta e a próxima linha precisam ser descomentadas para funcionar corretamente no Zapier
+        # conteudos_selecionados = [i.strip() for i in conteudos_selecionados]
+        conteudos = get_contents(token)
+        member_id = identify(email)
+        print(f"Email existente: {email}. Identificador: {member_id}. \n 1/3 etapas concluídas")
+        enrollments = get_enrollments(token)
+        content_ids = find_id_by_title(conteudos, conteudos_selecionados)
+        member_enrollments = identify_users_enrollments(enrollments, member_id=member_id, content_ids=content_ids)
+        print(f"Matrículas identificadas: {member_enrollments} \n 2/3 etapas concluídas")
+        revoked_contents = revoke_all(enrollments=member_enrollments, token=token)
+        print(f"Os conteúdos: \n {conteudos_selecionados} \n foram bloqueados para o aluno {email} identificador {member_id} \n 3/3 etapas concluídas")
+    except KeyError as e:
+        print(e)
